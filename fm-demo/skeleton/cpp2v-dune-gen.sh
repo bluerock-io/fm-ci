d2dee7b787c940e1734bdcddfb647131923f803d
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2025-05-31 01:28:52 +0200

    Automated changes: 's/BlueRock Security,? Inc\./BlueRock Security, Inc./g'

--- fm-demo/skeleton/cpp2v-dune-gen.sh
+++ fm-demo/skeleton/cpp2v-dune-gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) BlueRock Security Inc. 2020-2025
+# Copyright (C) BlueRock Security, Inc. 2020-2025
 #
 # This software is distributed under the terms of the BedRock Open-Source License.
 # See the LICENSE-BedRock file in the repository root for details.
